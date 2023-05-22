3c40e007acc42b1c09734a0b6b78ab42547b2112
Eirik Klebo <eiriktobias@gmail.com>
2023-05-22 12:57:32 +0200

    created domElements

--- js/main.js
+++ js/main.js
@@ -5,41 +5,64 @@ class Player {
     this.positionX = 45;
     this.positionY = 10;
   }
-}
+
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.id = "player";
+    this.domElement.style.width = this.width;
+    this.domElement.style.height = this.height;
+    this.domElement.style.left = this.positionX;
+    this.domElement.style.down = this.positionY;
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+
+  //Players movement
+  moveLeft() {
+    this.positionX = this.positionX - 2;
+    this.domElement.style.left = this.positionX + "vw";
+  }
+  moveLeft() {
+    this.positionX = this.positionX + 2;
+    this.domElement.style.left = this.positionX + "vw";
+  }
+  moveUp() {
+    this.positionY = this.positionX + 2;
+    this.domElement.style.up = this.positionY + "vh";
+  }
+  moveDown() {
+    this.positionY = this.positionX - 2;
+    this.domElement.style.down = this.positionY + "vh";
+  }
+} // E N D  C L A S S  P L A Y E R
 
 const player = new Player();
 
-createDomElement() {
-    this.domElement = document.createElement("div");
-    this.domElement = "player";
-}
-//Players movement
-moveLeft() {
-    this.positionX = this.positionX - 2;
-    this.domElement.style.left = this.positionX + "vw";
-}
-
-moveLeft() {
-    this.positionX = this.positionX + 2;
-    this.domElement.style.left = this.positionX + "vw";
-}
-
-moveUp() {
-    this.positionY = this.positionX + 2;
-    this.domElement.style.up = this.positionY + "vh";
-}
-moveDown() {
-    this.positionY = this.positionX - 2;
-    this.domElement.style.up = this.positionY + "vh";
-}
-
 class Enemy {
-    constructor() {
-        this.width = 10;
-        this.height = 10;
-        this.positionX = 45;
-        this.positionY = 100;
-    }
-}
+  constructor() {
+    this.width = 10;
+    this.height = 10;
+    this.positionX = 45;
+    this.positionY = 100;
+    this.domElement = null;
+    this.createDomElement();
+  }
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.className = "enemy";
+    this.domElement.style.width = this.width;
+    this.domElement.style.height = this.height;
+    this.domElement.style.left = this.positionX;
+    this.domElement.style.down = this.positionY;
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+  moveDown() {
+    this.positionY = this.positionX - 1;
+    this.domElement.style.down = this.positionY + "vh";
+  }
+} // E N D  C L A S S  E N E M Y
 
 const enemy = new Enemy();
