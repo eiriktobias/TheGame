29ac82632488b6d2d3447eeb33e9ae9c211367fa
Eirik Klebo <eiriktobias@gmail.com>
2023-05-22 11:32:47 +0200

    created enemy

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ class Player {
 }
 
 const player = new Player();
+
+
+
+class Enemy {
+    constructor() {
+        this.width = 10;
+        this.height = 10;
+        this.positionX = 45;
+        this.positionY = 100;
+    }
+}
+
+const enemy = new Enemy();
