f8f04a1edab6c53afe5a3f599b2bfbf154d55380
Eirik Klebo <eiriktobias@gmail.com>
2023-05-23 12:28:50 +0200

    created gameover screen. Added enemy 3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Player {
   constructor() {
-    this.width = 99;
-    this.height = 100;
-    this.positionX = 35;
-    this.positionY = 10;
+    this.width = 5.5;
+    this.height = 6;
+    this.positionX = 2;
+    this.positionY = 2;
 
     this.domElement = null;
     this.createDomElement();
@@ -12,10 +12,10 @@ class Player {
   createDomElement() {
     this.domElement = document.createElement("div");
     this.domElement.id = "player";
-    this.domElement.style.width = this.width + "px";
-    this.domElement.style.height = this.height + "px";
-    this.domElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.width = this.width + "rem";
+    this.domElement.style.height = this.height + "rem";
+    this.domElement.style.left = this.positionX + "rem";
+    this.domElement.style.bottom = this.positionY + "rem";
 
     const parentElm = document.getElementById("board");
     parentElm.appendChild(this.domElement);
@@ -24,29 +24,32 @@ class Player {
   //Players movement
   moveLeft() {
     this.positionX = this.positionX - 2;
-    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.left = this.positionX + "rem";
   }
   moveRight() {
     this.positionX = this.positionX + 2;
-    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.left = this.positionX + "rem";
   }
   moveUp() {
     this.positionY = this.positionY + 2;
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.bottom = this.positionY + "rem";
   }
   moveDown() {
     this.positionY = this.positionY - 2;
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.bottom = this.positionY + "rem";
   }
+  shoot() {}
 } // E N D  C L A S S  P L A Y E R
 
 const player = new Player();
 
 class Enemy {
   constructor() {
-    this.width = 100;
-    this.height = 100;
-    this.positionX = 45;
+    this.width = 6.2;
+    this.height = 6;
+    this.positionX = Math.floor(
+      Math.random() * (45 - this.width + 1)
+    ); /* 0 .... 100-w */
     this.positionY = 100;
     this.domElement = null;
     this.createDomElement();
@@ -54,17 +57,17 @@ class Enemy {
   createDomElement() {
     this.domElement = document.createElement("div");
     this.domElement.className = "enemy";
-    this.domElement.style.width = this.width + "px";
-    this.domElement.style.height = this.height + "px";
-    this.domElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.width = this.width + "rem";
+    this.domElement.style.height = this.height + "rem";
+    this.domElement.style.left = this.positionX + "rem";
+    this.domElement.style.bottom = this.positionY + "rem";
 
     const parentElm = document.getElementById("board");
     parentElm.appendChild(this.domElement);
   }
   moveDown() {
-    this.positionY = this.positionY - 1;
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.positionY = this.positionY - 0.2;
+    this.domElement.style.bottom = this.positionY + "rem";
   }
 } // E N D  C L A S S  E N E M Y
 
@@ -76,10 +79,131 @@ setInterval(() => {
 }, 3500);
 
 setInterval(() => {
-  enemyArray.forEach((enemyInstance) => {
-    enemyInstance.moveDown();
+  enemyArray.forEach((collision) => {
+    collision.moveDown();
+
+    if (
+      collision.positionX < player.positionX + player.width &&
+      collision.positionX + collision.width > player.positionX &&
+      collision.positionY < player.positionY + player.height &&
+      collision.height + collision.positionY > player.positionY
+    ) {
+      console.log("game over");
+      location.href = "./gameover.html";
+    }
   });
-}, 60);
+}, 7);
+
+// ENEMY 2
+class Enemy2 {
+  constructor() {
+    this.width = 6;
+    this.height = 6.3;
+    this.positionX = Math.floor(Math.random() * (45 - this.width + 1));
+    this.positionY = 100;
+    this.domElement = null;
+    this.createDomElement();
+  }
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.className = "enemy2";
+    this.domElement.style.width = this.width + "rem";
+    this.domElement.style.height = this.height + "rem";
+    this.domElement.style.left = this.positionX + "rem";
+    this.domElement.style.bottom = this.positionY + "rem";
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+  moveDown() {
+    this.positionY = this.positionY - 0.1;
+    this.domElement.style.bottom = this.positionY + "rem";
+  }
+} // E N D  C L A S S  E N E M Y
+
+const enemy2Array = [];
+
+///////////////////////////////////////////////////////////////
+
+setInterval(() => {
+  const newEnemy2 = new Enemy2();
+  enemy2Array.push(newEnemy2);
+}, 7500);
+
+setInterval(() => {
+  enemy2Array.forEach((collision) => {
+    collision.moveDown();
+
+    if (
+      collision.positionX < player.positionX + player.width &&
+      collision.positionX + collision.width > player.positionX &&
+      collision.positionY < player.positionY + player.height &&
+      collision.height + collision.positionY > player.positionY
+    ) {
+      console.log("game over");
+      location.href = "./gameover.html";
+    }
+  });
+}, 10);
+
+// ENEMY 2
+class Enemy3 {
+  constructor() {
+    this.width = 6.2;
+    this.height = 6.1;
+    this.positionX = Math.floor(Math.random() * (45 - this.width + 1));
+    this.positionY = 100;
+    this.domElement = null;
+    this.createDomElement();
+  }
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.className = "enemy3";
+    this.domElement.style.width = this.width + "rem";
+    this.domElement.style.height = this.height + "rem";
+    this.domElement.style.left = this.positionX + "rem";
+    this.domElement.style.bottom = this.positionY + "rem";
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+  moveDown() {
+    this.positionY = this.positionY - 0.1;
+    this.domElement.style.bottom = this.positionY + "rem";
+  }
+} // E N D  C L A S S  E N E M Y
+
+const enemy3Array = [];
+
+///////////////////////////////////////////////////////////////
+
+setInterval(() => {
+  const newEnemy3 = new Enemy3();
+  enemy3Array.push(newEnemy3);
+}, 6000);
+
+setInterval(() => {
+  enemy3Array.forEach((collision) => {
+    collision.moveDown();
+
+    if (
+      collision.positionX < player.positionX + player.width &&
+      collision.positionX + collision.width > player.positionX &&
+      collision.positionY < player.positionY + player.height &&
+      collision.height + collision.positionY > player.positionY
+    ) {
+      console.log("game over");
+      location.href = "./gameover.html";
+    }
+  });
+}, 5);
+
+class Bullet {
+  constructor() {
+    this.width = 1;
+    this.height = 1.5;
+  }
+}
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "ArrowLeft") {
@@ -90,5 +214,11 @@ document.addEventListener("keydown", (event) => {
     player.moveUp();
   } else if (event.code === "ArrowDown") {
     player.moveDown();
+  } else if (event.code === " ") {
+    player.shoot();
   }
 });
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// T  E  S  T /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
